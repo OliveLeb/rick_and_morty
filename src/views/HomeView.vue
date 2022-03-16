edd6436d5722518d48c909dcdbecc315f5ef8fb3
OliveLeb <lebel.olivier@wanadoo.fr>
2022-03-16 13:51:00 +0100

    delete base component and install tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="text-red-500">Home</h1>
 </template>
